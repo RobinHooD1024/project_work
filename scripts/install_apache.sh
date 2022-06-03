b700dcd10c3c9a4bbb4c8423e665b1d0770cecfc
Andrey Teterin <robinhood.ex@gmail.com>
2022-06-02 23:42:35 -0400

    update

--- scripts/install_apache.sh
+++ scripts/install_apache.sh
@@ -8,5 +8,6 @@ yum install -y epel-release;
 yum install -y httpd
 cp -r ../sites_examples/html ../sites_examples/html1 ../sites_examples/html2 ../sites_examples/html3 /var/www/
 cp ../configs/apache/httpd.conf /etc/httpd/conf/
+cp ../configs/apache/welcome.conf /etc/httpd/conf.d/
 systemctl start httpd
 systemctl status httpd
