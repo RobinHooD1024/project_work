f3e26ca86bf1b6530e258e3093964b9076d3faab
Andrey Teterin <robinhood.ex@gmail.com>
2022-06-03 22:31:56 -0400

    update

--- scripts/install_mysql.sh
+++ scripts/install_mysql.sh
@@ -3,10 +3,8 @@
 rpm -Uvh https://repo.mysql.com/mysql80-community-release-el7-3.noarch.rpm
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
 rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
-yum --enablerepo=mysql80-community install mysql-community-server
+yum --enablerepo=mysql80-community install -y mysql-community-server
 systemctl start mysqld
 systemctl enable mysqld
 grep "A temporary password" /var/log/mysqld.log
 mysql_secure_installation
-
-
---
+++ scripts/run_master.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PATH=$PATH:/usr/local/bin
+
+MYSQL='mysql --skip-column-names -uroot -p'Justnewpassword1!''
+
+$MYSQL -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'slaveTestPassword3%'; "
+$MYSQL -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%'; "
+$MYSQL -e "SELECT User, Host FROM mysql.user; "
+$MYSQL -e "SHOW MASTER STATUS; "
