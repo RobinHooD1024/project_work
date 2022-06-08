769c27ae74a624caac2cfc5779bb3f78f20030c4
Andrey Teterin <robinhood.ex@gmail.com>
2022-06-07 23:18:23 -0400

    update

--- scripts/backup_sql.sh
+++ scripts/backup_sql.sh
@@ -15,7 +15,7 @@ $MYSQL -e "STOP SLAVE;"
 for s in mysql `$MYSQL -e "SHOW DATABASES LIKE '%'"`;
     do
         mkdir $s;
-        for z in mysql `$MYSQL -e "SHOW TABLES FROM $s;"
+        for z in mysql `$MYSQL -e "SHOW TABLES FROM $s;"`;
 		do
 			/usr/bin/mysqldump -uroot -p'Justnewpassword1!' --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers --master-data=2 $s $z | gzip -1 > $s/$z.gz;
 		done
