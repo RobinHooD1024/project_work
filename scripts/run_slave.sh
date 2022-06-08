6406555e8e58e48cfdc4d8604151aab2bbad4121
Andrey Teterin <robinhood.ex@gmail.com>
2022-06-07 22:49:33 -0400

    update

--- scripts/run_slave.sh
+++ scripts/run_slave.sh
@@ -10,5 +10,3 @@ MYSQL='mysql --skip-column-names -uroot -p'Justnewpassword1!''
 $MYSQL -e "CHANGE MASTER TO MASTER_HOST='45.33.87.57', MASTER_USER='repl', MASTER_PASSWORD='slaveTestPassword3%', MASTER_LOG_FILE='binlog.000001', MASTER_LOG_POS=1330, GET_MASTER_PUBLIC_KEY = 1;"
 $MYSQL -e "START SLAVE; "
 $MYSQL -e "SHOW SLAVE STATUS\G; "
- 
-sss
